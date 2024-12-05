51a32ab62d8108c53a98563097591b0daab095f5
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-05 11:54:55 +0300

    Add documentation

--- InfinityFeed/InfinityFeed/Screens/Feed/Model/Anime.swift
+++ InfinityFeed/InfinityFeed/Screens/Feed/Model/Anime.swift
@@ -1,9 +1,17 @@
 import UIKit
 
+/// Struct to hold an info about each of the specific anime we display on the feed
 struct Anime: Hashable {
-
+	
+	/// Unique ID
 	let id: UUID
+	
+	/// Name of the anime
 	let title: String
+	
+	/// Description of the anime
 	let description: String?
+	
+	/// Poster image of the anime
 	let image: UIImage?
 }
