d66a8fc314f7e5cb1456271fad9a2397ff6b9e55
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-05 11:52:32 +0300

    Refactor AnimeLoader

--- InfinityFeed/InfinityFeed/Network/AnimeLoader.swift
+++ InfinityFeed/InfinityFeed/Network/AnimeLoader.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-
+/// Protocol for the service to handle data loading
 protocol AnimeLoaderProtocol: AnyObject {
 
 	var baseURL: String { get }
@@ -10,29 +10,42 @@ protocol AnimeLoaderProtocol: AnyObject {
 	func incrementPage()
 }
 
+/// Implementation of the service to handle data loading
 final class AnimeLoader: AnimeLoaderProtocol {
 
+	// MARK: - Public Properties
+
 	let baseURL = "https://kitsu.app/api/edge/anime"
+
+	// MARK: - Private Properties
 
 	private var currentPage = 0
 	private var perPage = 5
 
 	private(set) var session: URLSession
 
+	// MARK: - Initialization
+
 	init(session: URLSession = .shared) {
 		self.session = session
 	}
+}
+// MARK: - Public Methods
 
+extension AnimeLoader {
+
+	/// Method to track what page we already did load
 	func incrementPage() {
 		currentPage += 5
 	}
 
+	/// Main method to build an API path and load the data
 	func getData() async throws -> Response {
 
 		let urlString = "\(baseURL)?page[limit]=\(perPage)&page[offset]=\(currentPage)"
 
 		guard let url = URL(string: urlString) else {
-		throw URLError(.badURL)
+			throw URLError(.badURL)
 		}
 
 		let (data, _) = try await session.data(from: url)
